fa4bc065bd6edc6038733e488604ab3e82144375
ectrodev <ectroshop9@gmail.com>
2026-04-20 00:17:55 +0200

    Refactor service worker with eslint updates
    Updated eslint directives and removed unnecessary ts-ignore comments.

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,20 +1,16 @@
-/* eslint-disable no-restricted-globals */
+/* eslint-disable no-restricted-globals, no-undef */
+
 const CACHE_NAME = 'smart-hive-v1';
 const urlsToCache = ['/', '/index.html'];
 
-// @ts-ignore
 self.addEventListener('install', (event) => {
-  // @ts-ignore
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
 });
 
-// @ts-ignore
 self.addEventListener('fetch', (event) => {
-  // @ts-ignore
   event.respondWith(
-    // @ts-ignore
     caches.match(event.request).then((response) => response || fetch(event.request))
   );
 });
